d9b23949ef256f67745a935a335119bb48e919b6
lucasescucha <lucasmatiasbarrera@gmail.com>
2022-11-04 15:19:31 -0300

    Actualización de tests

--- test/testOffsetSolver.py
+++ test/testOffsetSolver.py
@@ -2,7 +2,7 @@ import unittest
 import math
 import numpy as np
 
-from solver.offsetSurfaceSolver import OffsetSurfaceSolver
+from solver.solverUtils import getNormalHat, getNormalVector, solveF_Factory
 
 class Test_TestOffsetSolver(unittest.TestCase):
 
@@ -10,17 +10,17 @@ class Test_TestOffsetSolver(unittest.TestCase):
         sqrt3 = math.sqrt(3)
 
         assert np.linalg.norm(
-            OffsetSurfaceSolver.getNormalVector(np.array([1, 1]))) == sqrt3
+            getNormalVector(np.array([1, 1]))) == sqrt3
         assert np.linalg.norm(
-            OffsetSurfaceSolver.getNormalVector(np.array([-1, -1]))) == sqrt3
+            getNormalVector(np.array([-1, -1]))) == sqrt3
         assert np.linalg.norm(
-            OffsetSurfaceSolver.getNormalVector(np.array([1, -1]))) == sqrt3
+            getNormalVector(np.array([1, -1]))) == sqrt3
         assert np.linalg.norm(
-            OffsetSurfaceSolver.getNormalVector(np.array([0, 0]))) == math.sqrt(1)
+            getNormalVector(np.array([0, 0]))) == math.sqrt(1)
 
     def test_getNormalHat(self):
         assert np.linalg.norm(
-           OffsetSurfaceSolver.getNormalHat(np.array([2, -5]))) == 1
+           getNormalHat(np.array([2, -5]))) == 1
 
     def test_solveF(self):
         sqrt3 = math.sqrt(3)
@@ -29,8 +29,9 @@ class Test_TestOffsetSolver(unittest.TestCase):
         Pp = np.array([1/sqrt3, 1/sqrt3, 1/sqrt3])
         grad = np.array([-1, -1])
 
-        assert np.linalg.norm(OffsetSurfaceSolver.solveF(
-            P[0:2], Pp[0:2], lambda x: grad[0:2], 1)) < 1e-5
+        solveF = solveF_Factory(Pp[0:2], lambda x: grad[0:2], 1)
+
+        assert np.linalg.norm(solveF(P[0:2])) < 1e-5
 
     def test_svgpathtools(self):
         assert True
