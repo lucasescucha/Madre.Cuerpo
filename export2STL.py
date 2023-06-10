3ecafe31b160ac22fe41fd998ed8a904f9f84985
lucasescucha <lucasmatiasbarrera@gmail.com>
2023-06-10 18:32:22 -0300

    Fix en exportación

--- export2STL.py
+++ export2STL.py
@@ -2,6 +2,7 @@ import os
 import shutil
 
 import FreeCADTools.utils as FreeCADUtils
+import Mesh
 
 document = FreeCADUtils.openDocument("output/output.FCStd")
 
@@ -13,6 +14,6 @@ os.makedirs("output/stl")
 for obj in document.Objects:
     print("Exportando objeto", obj.Label)
     filename = "output/stl/" + obj.Label + ".stl"
-    obj.Shape.exportStl(filename)    
+    Mesh.export([obj], filename)
 
 input("Presione una tecla para terminar...")
